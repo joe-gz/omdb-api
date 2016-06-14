4930b71bd66054efb4a47ae89a22b33249b91a34
Adrian Maseda <amaseda@gmail.com>
2016-06-14 10:03:36 -0400

    remove solution from starter

--- js/script.js
+++ js/script.js
@@ -1,26 +1,6 @@
-
 // initial API call
 $(document).ready( function() {
   $(".search").click(function(){
-    apiCall();
-  })
-})
-
-
-var apiCall = function() {
-  var searchText = $("#keyword").val();
-  var OMDBurl = "https://www.omdbapi.com/?t="+ searchText;
-
-  var config = {
-    url: OMDBurl,
-    type: "GET",
-    dataType: "json"
-  };
-
-  $.ajax(config)
-    .done(displayResults)
-}
-
-function displayResults(response){
- console.log(response);
-}
+    // Insert API call here...
+  });
+});
